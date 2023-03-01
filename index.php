e00ad63f62b93e61a36f35de5cfb11e88ee4772d
Taris Henrique <taris.tj@hotmail.com>
2023-02-28 21:11:41 -0300

    Aula 28-02-2023

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ and open the template in the editor.
         <script src="js/bootstrap.min.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-
+        <script src="./js/funcoes.js" type="text/javascript"> </script>
         <!--------------------------------------------->
     </head>
     <body>
